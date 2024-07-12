c9bdb1c865dee991dfda1c2e9e1b148d43f7d5c3
lukicusi <lukicusi@gmail.com>
2024-07-12 07:51:57 -0500

    having issues with task 2- throwing toUpper is undefined

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -33,26 +33,73 @@ function oldScrabbleScorer(word) {
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some scrabble!");
+ let task1Word = input.question("Enter a word to score: "); 
+ console.log(oldScrabbleScorer(task1Word));
 };
 
 let newPointStructure;
 
-let simpleScorer;
+function simpleScorer (word) { //LUCAS i think done, or add more after accumulator like oldScrabbleScorer example
+   word = word.toUpperCase();
+	let simplePoints = "";
+	for (let i = 0; i < word.length; i++) {
+      simplePoints += 1;
+   }
+   return simplePoints;
+};
 
-let vowelBonusScorer;
+let vowelBonusScorer = function(word) {
+   word = word.toUpperCase();
+	let vowelStructurePts = "";
+   for (let i = 0; i < word.length; i++) {
+      if (word.includes(['a', 'e', 'i', 'o', 'u'])) { //LUCAS i didnt include 'Y'
+			vowelStructurePts += 3;
+		 }else if (word.includes(['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])) {
+         vowelStructurePts += 3;
+       } //LUCASSSSSSSSSSSSSSSSS should i add an else statement here for " " or nums?
+   }
+   return vowelStructurePts;
+};
 
 let scrabbleScorer;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+   {
+      "name": "Simple Score",
+      "description": "Each letter is worth 1 point.",
+      "scorerFunction": simpleScorer() //LUCAS functions at these places correct? or need to call function() ?
+   },
+   {
+      "name": "Bonus Vowels",
+      "description": "Vowels are 3 pts, consonants are 1 pt.",
+      "scorerFunction": vowelBonusScorer() //LUCAS do these need ';'? or maybe even pass in (word)
+   },
+   {
+      "name": "Scrabble",
+      "description": "The traditional scoring algorithm.",
+      "scorerFunction": oldScrabbleScorer()
+   }
+]; //Lucas idk if i need this ";"
 
-function scorerPrompt() {}
+function scorerPrompt() { //LUCAS i dont think this needs a parameter
+   let gradingChoice = input.question("Enter 0, 1, or 2 for a scoring algorithm: ");
+   if (gradingChoice === 0) { //Lucas is this correct with ===?
+      console.log(simpleScorer(word)); //LUCAS is the () word, tast1word, or a new variable?
+   }else if (gradingChoice === 1) {
+      console.log(vowelBonusScorer(word));
+   }else if (gradingChoice === 2) {
+      console.log(oldScrabbleScorer(word));
+   } //LUCAS maybe do else here and log "invalid try again" and loop it back to prompt
+   return gradingChoice; //LUCAS do i pass in (word)?
+} //;LUCAS does this need ';'?
+
 
 function transform() {};
 
 function runProgram() {
    initialPrompt();
-   
+   scorerPrompt(); ///LUCAS DO I NEED MORE HERE TO CALL AND RUN THIS?
 }
 
 // Don't write any code below this line //
